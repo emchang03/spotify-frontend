80f11a3a7cbc6e646ad363de59e2e67b33bd658d
emilychang <emily.chang@vanderbilt.edu>
2023-06-05 21:57:16 -0700

    changed redirect url

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -25,7 +25,7 @@ const Home = () =>{
             <div className="center-container">
                 <div>
                     <h2>welcome to your <p className="inline" style={{color: "#8D0688"}}> music moodring</p></h2>
-                    <a className="link" href="http://localhost:8888/login"> 
+                    <a className="link" href="https://moodring-a5dn.onrender.com/login"> 
                         log in to Spotify
                     </a>
                 </div>
